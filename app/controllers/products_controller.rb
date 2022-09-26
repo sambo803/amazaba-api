017326170d7729d0543e751a807bf0aaa8e11555
samuel <sbacon803@gmail.com>
2022-09-26 16:52:42 -0600

    add create action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,15 @@ class ProductsController < ApplicationController
     products = Product.all
     render json: products
   end
+
+  def create
+    product = Product.new(
+      name: params[:name],
+      description: params[:description],
+      price: params[:price],
+      image_url: params[:image_url]
+    )
+    product.save
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/products" => "products#index"
+  post "/products" => "products#create"
+  
 end
